e0e8af989e212d280ea797b83b38fdac2f33c205
jonatas-melo-silva <jonatas891024@gmail.com>
2021-08-26 18:10:51 -0300

    :sparkles: Adicionado paginas do app

---
+++ src/pages/agenda.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Template from '../components/templates/Agenda'
+
+const Agenda: NextPage = () => {
+  return (
+    <div>
+      <Template/>
+    </div>
+  )
+}
+
+export default Agenda;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Template from '../components/templates/Home'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Template/>
+    </div>
+  )
+}
+
+export default Home
