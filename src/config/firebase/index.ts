3aa343913bc68b5415ba2e6157a936bc0266e389
jonatas-melo-silva <jonatas891024@gmail.com>
2021-08-26 22:00:35 -0300

    :sparkles: Adicionado configuraçoes para o firebase

---
+++ src/config/firebase/index.ts
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const FirebaseConfig = {
+  
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+const FireBase = firebase.apps.length
+? firebase.app()
+: firebase.initializeApp(FirebaseConfig);
+
+export const PersistenceMode = firebase.auth.Auth.Persistence.SESSION;
+
+// Initialize Firebase
+export default FireBase;
