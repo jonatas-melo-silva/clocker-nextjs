5a73192c651ecb8392a11da2707593cefe67aa8e
jonatas-melo-silva <jonatas891024@gmail.com>
2021-08-27 11:48:49 -0300

    :sparkles: Adicionado injetando useAuthContext no app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+
+import AuthUserProvider from '../contexts/AuthUserContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <AuthUserProvider>
+        <Component {...pageProps} />
+      </AuthUserProvider>
     </ChakraProvider>
-  )}
-export default MyApp
+  );
+}
+export default MyApp;
